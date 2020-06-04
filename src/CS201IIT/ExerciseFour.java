56d6c112130b45db6554cc4026e2a18cb578b6e8
nalnatsheh@hawk.iit.edu <Nader171019979797$&>
2020-06-03 23:11:03 -0600

    git clone https://NaderAlnatsheh@bitbucket.org/dboliske/cs201-202006-nalnatsheh.git

---
+++ src/CS201IIT/ExerciseFour.java
@@ -0,0 +1,11 @@
+package CS201IIT;
+
+public class ExerciseFour {
+    public static void main(String [] args){
+        byte x = 127;
+        byte y = 127;
+
+        int square = x * y;
+        System.out.println("The area of the square is " + square);
+    }
+}
---
+++ src/CS201IIT/ExerciseThree.java
@@ -0,0 +1,7 @@
+package CS201IIT;
+
+public class ExerciseThree {
+    public static void main(String [] args){
+        System.out.println("My name is Nader Alnatsheh and my birthdate is Oct. 17, 1997.");
+    }
+}
